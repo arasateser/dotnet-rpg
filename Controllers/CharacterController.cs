5541b3c821378e79f28a19e08938265b10e4d967
Aras Ateser <43793683+arasateser@users.noreply.github.com>
2023-08-22 13:34:06 +0300

    Asynchronous Calls update

---
+++ Controllers/CharacterController.cs
@@ -0,0 +1,32 @@
+global using dotnet_rpg.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_rpg.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CharacterController : ControllerBase
+    {
+        private static List<Character> characters = new List<Character>{
+            new Character(),
+
+            new Character{Id =1, Name = "Sam"}
+    };
+
+        [HttpGet("Get All")]
+        public ActionResult<List<Character>> Get()
+        {
+            return Ok(characters);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<List<Character>> GetSingle(int id)
+        {
+            return Ok(characters.FirstOrDefault(character => character.Id == id));
+        }
+    }
+}
