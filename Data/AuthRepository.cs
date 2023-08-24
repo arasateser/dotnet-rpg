02b14dea6c8daa5a90ec985344b60d1ac43c8db8
Aras Ateser <43793683+arasateser@users.noreply.github.com>
2023-08-24 15:50:54 +0300

    UserExist Method

--- Data/AuthRepository.cs
+++ Data/AuthRepository.cs
@@ -20,20 +20,30 @@ namespace dotnet_rpg.Data
 
         public async Task<ServiceResponse<int>> Register(User user, string password)
         {
+            var response = new ServiceResponse<int>();
+            if (await UserExist(user.Username))
+            {
+                response.Success = false;
+                response.Message = "User Already Exist";
+                return response;
+            }
+
             CreatePasswordHash(password, out byte[] passwordHash, out byte[] passwordSalt);
             user.PasswordHash = passwordHash;
             user.PasswordSalt = passwordSalt;
 
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
-            var response = new ServiceResponse<int>();
             response.Data = user.Id;
             return response;
         }
 
-        public Task<bool> UserExist(string username)
+        public async Task<bool> UserExist(string username)
         {
-            throw new NotImplementedException();
+            if (await _context.Users.AnyAsync(u => u.Username.ToLower() == username.ToLower()))
+                return true;
+
+            return false;
         }
 
         private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
